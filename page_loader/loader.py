f7d22a9993d45deb1b1a88795dfc4036c0f07a19
grins <potemkuh@gmail.com>
2021-09-28 12:54:39 +0300

    add logs

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -1,13 +1,13 @@
 import os.path
 import requests
-import re
 from page_loader.download_asset import download_assets
-from urllib.parse import urlparse
 from page_loader.get_name import get_filename, get_dirname
+import logging
 
 
 def download(link, dir=None):
     html = requests.get(link)
+    html.raise_for_status()
     html = html.text
 
     #получение имени файла и дериктории
@@ -27,4 +27,7 @@ def download(link, dir=None):
     with open(path, 'w') as f:
         f.write(updated_html)
     print(path)
+
+    logging.info('requested url: {0}'.format(link))
+    logging.info('output path: {0}'.format(full_path))
     return path
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,10 +1,17 @@
 from page_loader.loader import download
 from page_loader.parser import parse
+import logging
+import sys
 
 
 def main():
-    args = parse()
-    download(args.url, args.output)
+    try:
+        args = parse()
+        logging.basicConfig(level=logging.INFO)
+        logging.info('Start download')
+        download(args.url, args.output)
+    except:
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
